540aaea6dd88b9535216ac77e5a055a12fab209f
Alabaii <48452442+Alabaii@users.noreply.github.com>
2024-12-08 18:51:29 +0300

    rework CreateAnimalService and CreateAnimalServiceImpl

--- src/CreateAnimalService.java
+++ src/CreateAnimalService.java
@@ -1,8 +1,31 @@
 package src;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface CreateAnimalService {
-    List<Animal> createDefaultAnimals(); // Создать 10 животных (while)
-    List<Animal> createAnimals(int n);   // Создать N животных (for)
-    List<Animal> createAnimalsDoWhile(int n); // Создать N животных (do-while)
+    // Дефолтный метод, использующий цикл while для создания 10 животных
+    default List<Animal> createDefaultAnimals() {
+        return createAnimalsWhile(10);
+    }
+
+    // Метод для создания произвольного количества животных (с использованием цикла for)
+    List<Animal> createAnimals(int n);
+
+    // Метод для создания произвольного количества животных (с использованием цикла do-while)
+    List<Animal> createAnimalsDoWhile(int n);
+
+    // Вспомогательный метод для создания животных с использованием while
+    default List<Animal> createAnimalsWhile(int n) {
+        List<Animal> animals = new ArrayList<>();
+        int count = 0;
+        while (count < n) {
+            animals.add(generateAnimal(count));
+            System.out.println("Создано животное: " + animals.get(count).getName());
+            count++;
+        }
+        return animals;
+    }
+
+    // Метод для генерации животных
+    Animal generateAnimal(int id);
 }
--- src/CreateAnimalServiceImpl.java
+++ src/CreateAnimalServiceImpl.java
@@ -1,34 +1,18 @@
 package src;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class CreateAnimalServiceImpl implements CreateAnimalService {
 
     @Override
-    public List<Animal> createDefaultAnimals() {
-        List<Animal> animals = new ArrayList<>();
-        int count = 0;
-
-        while (count < 10) {
-            animals.add(generateAnimal(count));
-            System.out.println("Создано животное: " + animals.get(count).getName());
-            count++;
-        }
-
-        return animals;
-    }
-
-    @Override
     public List<Animal> createAnimals(int n) {
         List<Animal> animals = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             animals.add(generateAnimal(i));
             System.out.println("Создано животное: " + animals.get(i).getName());
         }
-
         return animals;
     }
 
@@ -36,17 +20,16 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
     public List<Animal> createAnimalsDoWhile(int n) {
         List<Animal> animals = new ArrayList<>();
         int count = 0;
-
         do {
             animals.add(generateAnimal(count));
             System.out.println("Создано животное: " + animals.get(count).getName());
             count++;
         } while (count < n);
-
         return animals;
     }
 
-    private Animal generateAnimal(int id) {
+    @Override
+    public Animal generateAnimal(int id) {
         // Генерация животных по порядку
         switch (id % 4) {
             case 0: return new Wolf("WolfBreed" + id, "Wolf" + id, 500.0, "Brave", LocalDate.of(2020, 1, 1));
@@ -56,3 +39,4 @@ public class CreateAnimalServiceImpl implements CreateAnimalService {
         }
     }
 }
+
